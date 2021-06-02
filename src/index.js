828c29c4e6a8be004ea9b037962f03bd220e1ff5
iwood55 <isaac.ewood@icloud.com>
2021-06-02 16:25:40 -0400

    mvp complete

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
-
+import useDarkMode from './components/hooks/useDarkMode'
 
 import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode();
 
   useEffect(() => {
     axios
