d5d71edfb632f84fe4456209c585399acecc0441
iwood55 <isaac.ewood@icloud.com>
2021-07-07 16:29:22 -0400

    flex mvp done

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,8 @@
+import useLocalStorage from './useLocalStorage';
+
+const useDarkMode= ()=>{
+    const [darkMode, setDarkMode] = useLocalStorage('DarkToggle', false)
+    return [darkMode, setDarkMode]
+};
+
+export default useDarkMode;
